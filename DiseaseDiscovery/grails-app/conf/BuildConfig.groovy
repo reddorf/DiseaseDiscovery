ee546a4eceb850cdac445812fbfd4f24d341a915
Marc <wrrzag666@gmail.com>
2014-02-13 18:31:20 +0100

    Added Spring Security plugin and user/role classes

--- DiseaseDiscovery/grails-app/conf/BuildConfig.groovy
+++ DiseaseDiscovery/grails-app/conf/BuildConfig.groovy
@@ -36,6 +36,7 @@ grails.project.dependency.resolution = {
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
+		mavenRepo "http://repo.spring.io/milestone/"
     }
 
     dependencies {
@@ -58,6 +59,7 @@ grails.project.dependency.resolution = {
 
         runtime ":database-migration:1.3.2"
 
-        compile ':cache:1.0.1'
+        compile ':cache:1.0.1',
+				':spring-security-core:2.0-RC2'
     }
 }
--- DiseaseDiscovery/grails-app/conf/Config.groovy
+++ DiseaseDiscovery/grails-app/conf/Config.groovy
@@ -89,3 +89,19 @@ log4j = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'auth.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'auth.UserRole'
+grails.plugin.springsecurity.authority.className = 'auth.Role'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	'/':                              ['permitAll'],
+	'/index':                         ['permitAll'],
+	'/index.gsp':                     ['permitAll'],
+	'/**/js/**':                      ['permitAll'],
+	'/**/css/**':                     ['permitAll'],
+	'/**/images/**':                  ['permitAll'],
+	'/**/favicon.ico':                ['permitAll']
+]
+

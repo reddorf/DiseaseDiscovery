796abb159fa90b91a7f58fded6233e392c6184ca
Marc Piñol <wrrzag666@gmail.com>
2014-02-14 00:38:52 +0100

    Added root user to bootstrap

--- DiseaseDiscovery/grails-app/conf/BootStrap.groovy
+++ DiseaseDiscovery/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,13 @@
+import auth.Role
+import auth.User
+import auth.UserRole
+
 class BootStrap {
 
     def init = { servletContext ->
+		def admin = new User(username: 'admin', password: '1234').save()
+		def adminRole = new Role(authority: 'ROLE_ADMIN').save()
+		def adminToRole = new UserRole(user: admin, role: adminRole).save()
     }
     def destroy = {
     }

a9dd60d33b59f051b7232fbbafcff0d154f71a91
Marc Piñol <wrrzag666@gmail.com>
2014-02-15 00:16:29 +0100

    Added domain model.

--- DiseaseDiscovery/grails-app/domain/com/Disease.groovy
+++ DiseaseDiscovery/grails-app/domain/com/Disease.groovy
@@ -1,7 +1,13 @@
 package com
 
 class Disease {
-
+	static hasMany = [symptom:Symptom]
+	
+	long id
+	String name
+	
     static constraints = {
+		id nullable: false
+		name blank: false, nullable: false
     }
 }
---
+++ DiseaseDiscovery/grails-app/domain/com/Symptom.groovy
@@ -0,0 +1,14 @@
+package com
+
+class Symptom {
+	static hasMany = [disease : Disease]
+	static belongsTo = Disease
+	
+	long id
+	String name
+	
+    static constraints = {
+		id nullable: false
+		name blank: false, nullable: false
+    }
+}
